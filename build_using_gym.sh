dcd0e34c9aae257da29e29ecc2c611a4a1c241b7
West <szhanfeng203@gmail.com>
2018-03-08 22:37:36 +0800

    打包脚本

---
+++ build_using_gym.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -ilex
+
+#设置超时
+export FASTLANE_XCODEBUILD_SETTINGS_TIMEOUT=120
+
+#计时
+SECONDS=0
+
+#假设脚本放置在与项目相同的路径下
+#project_path="$(dirname "$(pwd)")"
+project_path="$(pwd)"
+#取当前时间字符串添加到文件结尾
+now=$(date +"%Y_%m_%d_%H_%M_%S")
+projectName="VprScene"
+
+#指定项目的scheme名称
+scheme="VprScene"
+#指定要打包的配置名
+configuration="Release"
+#指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, development, 和developer-id，即xcodebuild的method参数
+export_method='ad-hoc'
+
+#指定项目地址
+workspace_path="$project_path/${projectName}.xcodeproj"
+#指定输出路径
+output_path="/Users/xhkj/Desktop/${projectName}"
+#指定输出归档文件地址
+archive_path="$output_path/${projectName}_${now}.xcarchive"
+#指定输出ipa地址
+ipa_path="$output_path/${projectName}_${now}.ipa"
+#指定输出ipa名称
+ipa_name="${projectName}_${now}.ipa"
+#获取执行命令时的commit message
+commit_msg="$1"
+
+#输出设定的变量值
+echo "===workspace path: ${workspace_path}==="
+echo "===archive path: ${archive_path}==="
+echo "===ipa path: ${ipa_path}==="
+echo "===export method: ${export_method}==="
+echo "===commit msg: $1==="
+
+# 解锁对login.keychain的访问，codesign会用到
+security unlock-keychain -p "123456" $HOME/Library/Keychains/login.keychain
+
+#react-native yarn 
+yarn install --registry http://192.168.0.9
+
+#cocoapods
+#pod install --no-repo-update
+
+
+#先清空前一次build
+fastlane gym --project ${workspace_path} --scheme ${scheme} --clean --configuration ${configuration} --archive_path ${archive_path} --export_method ${export_method} -allowProvisioningUpdates --output_directory ${output_path} --output_name ${ipa_name}
+
+#上传到pgyer
+#蒲公英上的User Key
+uKey="d0876958a45db884c287d85ab032bb01"
+#蒲公英上的API Key
+apiKey="8fdec42be987c001f6cf27b815cccbb4"
+#执行上传至蒲公英的命令
+echo "++++++++++++++upload+++++++++++++"
+curl -F "file=@${ipa_path}" -F "uKey=${uKey}" -F "_api_key=${apiKey}" https://qiniu-storage.pgyer.com/apiv1/app/upload
+
+#输出总用时
+echo "===Finished. Total time: ${SECONDS}s==="
